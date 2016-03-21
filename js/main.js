ba00ff6ccef053d061bbb61a8192308f9b0ef024
Kevin Ong <kevinyaohuiong@gmail.com>
2016-03-21 09:22:47 -0700

    Added Background choices

--- js/main.js
+++ js/main.js
@@ -6,15 +6,24 @@
 });*/
 
 var available_stamps = [];
+var available_backgrounds = [];
 var current_stamp = 0;
+var current_background = 0;
+var stamp_shapes = ["square", "horizontal", "vertical"]
 var maxscore = 0;
+var square = 0;
+var horizontal = 0;
+var vertical = 0;
 
+// Displays Options window when the Options icon is clicked
 $("#stamppick").on("click", function() {
+	$(".background_choice").fadeOut("fast");
 	$(".options").fadeOut("fast", function() {
 		$("#stamppickscreen").fadeIn("fast");
 	});		
 });
 
+// Select a new stamp by clicking on it, updates the stamp on the Preview page through setUserStampScreen(), and then closes the Options window
 $("#stamppickscreen").on("click", ".stampcontainer" ,function() {
 	if (available_stamps[$(this).data("stampindex")].unlocked) {
 		$(".stampcontainer").removeClass("selectedstamp");
@@ -25,42 +34,51 @@ $("#stamppickscreen").on("click", ".stampcontainer" ,function() {
 		setTimeout(function() {
 			$("#stamppickscreen").fadeOut("fast", function() {
 				$(".options").fadeIn("fast");
+				$(".background_choice").fadeIn("fast");
 			});		
 		}, 600);
 	}
-
 })
 
+$(".background_choice").on("click", ".background_icon", function() {
+	setBackground($(this).data("background"));
+})
+
+// Load backgrounds from backgrounds.json into variable available_backgrounds
+function loadBackgrounds() {
+	$.getJSON("./backgrounds.json", function(data) {
+		available_backgrounds = data.backgrounds;
+		//showBackgroundOptions();
+		//setBackground(current_background);
+		console.log(available_backgrounds);
+	})
+}
+
+function setBackground(image) {
+	$("html").css("background", "url(./images/" + image + ") no-repeat center center");
+}
+
+// Load stamps from stamps.json into variable available_stamps
 function loadStamps() {
 	$.getJSON("./stamps.json", function(data) {
 		available_stamps = data.stamps;
 		showStampOptions();
-		console.log(available_stamps);
 		setUserStampScreen(current_stamp);
 		unlockStamps();
 	})
 }
 
+// Shows which stamp is selected in the Options window, also shows the availability of unlocked stamps
 function showStampOptions() {
 	$("#stamppickscreen").empty();
 	for (var stamp in available_stamps) {
-		console.log(available_stamps[stamp]);
 		var isSelected = (stamp == current_stamp)?'selectedstamp':'';
 		var isLocked = (!available_stamps[stamp].unlocked)?'lockedstamp':'';
 		$("#stamppickscreen").append('<div data-stampindex="'+ stamp +'"class="stampcontainer pointer transition ' + isLocked + ' ' + isSelected + '"><h2>' + available_stamps[stamp].stamp_name + '</h2><img src="' + available_stamps[stamp].image + '"></div>');
 	}
-
 }
 
-function initGame() {
-	if (null != localStorage.getItem("current_stamp")) {
-		current_stamp = localStorage.getItem("current_stamp");
-	}
-	if (null != localStorage.getItem("maxscore")) {
-		maxscore = localStorage.getItem("maxscore");
-	}
-}
-
+// Changes the current stamp index from the Options window
 function setUserStamp(stamp_index) {
 	if (available_stamps[stamp_index].unlocked) {
 		localStorage.setItem("current_stamp", stamp_index);
@@ -68,12 +86,13 @@ function setUserStamp(stamp_index) {
 	}
 }
 
+// Displays the current selected stamp on the display page before entering the game
 function setUserStampScreen(stamp_index) {
-	console.log(available_stamps[stamp_index]);
 	$("#userStamp h2").text(available_stamps[stamp_index].stamp_name);
 	$("#userStamp img").attr("src", available_stamps[stamp_index].image);
 }
 
+// Determines which stamps are unlocked by iterating through each stamp and checking the maxscore against each stamp's unlocked score value
 function unlockStamps() {
 	for (var stamp in available_stamps) {
 		if (maxscore >= available_stamps[stamp].unlocked_score) {
@@ -83,6 +102,17 @@ function unlockStamps() {
 	showStampOptions();
 }
 
+// Initializes the current_stamp and maxscore variables from local storage
+function initGame() {
+	if (null != localStorage.getItem("current_stamp")) {
+		current_stamp = localStorage.getItem("current_stamp");
+	}
+	if (null != localStorage.getItem("maxscore")) {
+		maxscore = localStorage.getItem("maxscore");
+	}
+}
+
+// Sets the high score at the end of each game and runs unlockStamps()
 function endGame(score) {
 	var newHigh = false;
 
@@ -95,7 +125,9 @@ function endGame(score) {
 	return newHigh;
 }
 
+// Runs initGame(), loadStamps(), and loadBackgrounds() when page is loading
 $(function() {
 	initGame();
 	loadStamps();
+	loadBackgrounds();
 })
